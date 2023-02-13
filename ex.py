c71f77b542357dc36fb0c078c6b3b643fa5f9e47
hdliuder <whatdidhflearntoday@gmail.com>
2023-02-13 23:45:13 +0800

    sss

---
+++ ex.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  8 23:35:59 2022
+
+@author: hf
+"""
+import cv2
+import pyautogui as pag
+import PIL
+import numpy as np
+from time import sleep
+
+def fanwei(pt,anle):
+    for i in anle:
+        if abs(i[0] - pt[0]) < 10 and abs(i[1] - pt[1]) < 10:
+            return False
+    return True
+
+def find_ex_up():
+    img = cv2.imread('a.png', 0)
+    img2 = cv2.imread('aa.png', 0)
+    
+    bg = PIL.ImageGrab.grab(bbox=(0, 0, 1920, 1080))
+    bg = bg.crop((0,0,1300,1080))
+    bg.save('b.png')
+    bg = cv2.imread('b.png', 0)
+    # h, w = img.shape[:2]  # rows->h, cols->w
+    
+    method = cv2.TM_CCOEFF_NORMED
+    res = cv2.matchTemplate(img, bg, method)
+    res2= cv2.matchTemplate(img2, bg, method)
+    threshold = 0.9
+    loc = np.where(res >= threshold)
+    loc2 = np.where(res2 >= threshold)
+
+    locs = (np.concatenate((loc,loc2),axis=1))
+
+    
+    anle = []
+    for pt in zip(*locs[::-1]):  # *号表示可选参数
+        if fanwei(pt,anle) == True:
+            pag.click(pt[0],pt[1])
+            anle.append((pt[0],pt[1]))
+        # print(anle)
+        pag.click(1425,969)
+        sleep(0.1)
+        
+def scroll():
+    pag.moveTo(400,700)
+    pag.click(400,700)
+    pag.dragRel(0,-475,0.15)
+    pag.click(400,700)
+
+def run(n=100):
+    for i in range(n):
+        find_ex_up()
+        scroll()
+
+if __name__ == '__main__':
+    find_ex_up()
---
+++ hotkey.py
@@ -0,0 +1,132 @@
+import pyautogui as pag
+import keyboard
+import time
+import ex
+
+jing = 1
+pag.PAUSE = 0.01
+
+
+
+def typein():
+    pag.PAUSE = 0.1
+    pag.moveTo(412,52)
+    pag.click()
+    pag.hotkey('ctrl','a')
+    pag.typewrite(r'https://shinycolors.enza.fun/produce/?a=')
+    pag.press('enter')
+    pag.press('enter')
+    pag.PAUSE = 0.01
+    
+def typein2():
+    pag.PAUSE = 0.1
+    pag.moveTo(412,52)
+    pag.click()
+    pag.hotkey('ctrl','a')
+    pag.typewrite(r'https://shinycolors.enza.fun/produce/?a=#')
+    pag.press('enter')
+    pag.press('enter')
+    pag.PAUSE = 0.01
+    
+def typein0():
+    time.sleep(0.4)
+    pag.moveTo(412,52)
+    pag.click(clicks=3)
+    
+    pag.typewrite(r'https://shinycolors.enza.fun/produce/?a=')
+    pag.press('enter')
+    pag.press('enter')
+    time.sleep(1)
+    pag.click()
+    pag.press('right')
+    pag.typewrite(r'#')
+    pag.press('enter')
+    pag.press('enter')
+    pag.PAUSE = 0.01
+    
+def print_position():
+    print(pag.position())
+
+def switch():
+    global jing
+    if jing ==1:
+        pag.hotkey('alt','left')
+        jing = 0
+    else:
+        pag.hotkey('alt','right')
+        jing = 1
+    pag.moveTo(318,845)
+    time.sleep(0.1)
+
+def clicking():
+    while keyboard.is_pressed('z'):
+        pag.click()
+
+def go_da():
+    pag.moveTo(448,698)
+    pag.click()
+    pag.moveTo(806,765)
+
+def learn():
+    x,y = pag.position()
+    pag.moveTo(1670,963)
+    pag.click()
+    time.sleep(0.05)
+    pag.moveTo(1095,785)
+    pag.click()
+    pag.moveTo(x,y)
+
+
+
+def full():
+    pag.click()
+    time.sleep(1.25)
+    pag.click()
+    
+    
+def half2():
+    pag.click()
+    time.sleep(1.17)
+    pag.click()
+
+
+def dead():
+    pag.click()
+    time.sleep(1.05)
+    pag.click()
+
+def signup():
+    keyboard.add_hotkey('alt+x',unsign)
+    
+    keyboard.add_hotkey('alt+l',ex.run)
+    keyboard.add_hotkey('alt+1', typein0)
+    # keyboard.add_hotkey('alt+2', typein2)
+    keyboard.add_hotkey('p', print_position)
+    keyboard.add_hotkey('z', clicking)
+    keyboard.add_hotkey('x', switch)
+    keyboard.add_hotkey('c', go_da)
+    keyboard.add_hotkey('v', learn)
+    keyboard.add_hotkey('b', s1)
+    keyboard.add_hotkey('g', skip)
+    
+    keyboard.add_hotkey('q',full)
+    keyboard.add_hotkey('w',half2)
+    keyboard.add_hotkey('e',dead)
+
+def unsign():
+    keyboard.unhook_all_hotkeys()
+    keyboard.add_hotkey('alt+z',signup)
+    print('unsigned')
+    
+def s1():
+    pag.moveTo(990,926)
+    pag.click()
+
+def skip():
+    pag.moveTo(1722,146)
+    pag.click()
+
+print('run')
+signup()
+keyboard.add_hotkey('alt+x',unsign)
+print('inited')
