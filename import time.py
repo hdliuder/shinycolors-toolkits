d571ccfceaac8265f1b42a6ef47b5091cea9590d
hdliuder <whatdidhflearntoday@gmail.com>
2023-02-13 12:25:10 +0800

    高精度定时

---
+++ import time.py
@@ -0,0 +1,15 @@
+import time
+
+def delayMicrosecond(t):
+    start,end = 0,0
+    start = time.time()
+    t = (t-1000)/1000000
+    while end-start < t:
+        end = time.time()
+
+if __name__ == '__main__':
+    t = time.time()
+    print (str(int(round(t * 1000))))
+    delayMicrosecond(1000)
+    t = time.time()
+    print (str(int(round(t * 1000))))
